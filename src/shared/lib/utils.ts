920c5eeac520f506b96152b4eba3710a46be3723
kris3713 <07e5297d5b@c0x0.com>
2025-08-12 20:08:00 -0600

    Added documentation for the cn utility function

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * A helper function that helps with organizing CSS classes (including from Tailwind CSS)
+ *
+ * @param inputs CSS classes
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
