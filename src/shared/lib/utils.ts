b8ee767fbcb7f8ad63c81a075796026b1ad82546
kris3713 <07e5297d5b@c0x0.com>
2025-08-13 19:51:14 -0600

    Small update to `src/shared/lib/utils.ts`

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 /**
  * A helper function that helps with organizing CSS classes (including from Tailwind CSS)
